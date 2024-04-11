b36e5fc1442e2ce72128e50d384f7d95fa8621a8
malai <malai@plant.elesy.com>
2024-04-11 14:24:46 +0700

    Миграции

---
+++ Migrations/20240411072307_ComponentsCounter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaxDeiBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class ComponentsCounter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Count",
+                table: "ItemComponents",
+                type: "numeric(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "ItemComponents");
+        }
+    }
+}
