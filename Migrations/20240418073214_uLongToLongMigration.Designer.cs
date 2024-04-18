7de90072488f810f423bc6da3c99a66d61c7146d
malai <malai@plant.elesy.com>
2024-04-18 14:32:31 +0700

    Миграция

--- Migrations/20240418073214_uLongToLongMigration.Designer.cs
+++ Migrations/20240418073214_uLongToLongMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PaxDeiBot.Data;
@@ -11,9 +12,11 @@ using PaxDeiBot.Data;
 namespace PaxDeiBot.Migrations
 {
     [DbContext(typeof(ItemDbContext))]
-    partial class ItemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240418073214_uLongToLongMigration")]
+    partial class uLongToLongMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240418073214_uLongToLongMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaxDeiBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class uLongToLongMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Count",
+                table: "ItemComponents",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(20,0)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Count",
+                table: "ItemComponents",
+                type: "numeric(20,0)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
