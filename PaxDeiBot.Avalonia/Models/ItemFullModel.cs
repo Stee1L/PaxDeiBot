f77c700b8bd98d0286c51ba3b9a35aff71f9c330
malai <malai@plant.elesy.com>
2024-04-22 13:57:38 +0700

    Добавлен шаблон для моделей в Avalonia

---
+++ PaxDeiBot.Avalonia/Models/ItemFullModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AvaloniaApplication1.Models;
+
+public class ItemFullModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<ItemShortModel> Children { get; set; }
+    public List<ItemShortModel> Parents { get; set; }
+}
---
+++ PaxDeiBot.Avalonia/Models/ItemShortModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AvaloniaApplication1.Models;
+
+public class ItemShortModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
